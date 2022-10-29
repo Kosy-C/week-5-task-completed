2353bf05f025e37202002e9fb0ef30129d8c9c25
Kosy-C <kosychinweuba@gmail.com>
2022-10-29 01:20:24 +0100

    last commit

--- server2/lib/app.js
+++ server2/lib/app.js
@@ -3,48 +3,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const axios_1 = __importDefault(require("axios"));
-let cheerio = require("cheerio");
-// import cheerio from "cheerio"
 const http_1 = __importDefault(require("http"));
-const server = http_1.default.createServer((req, res) => {
-    if (req.method === "POST") {
-        let body = "";
-        req.on("data", (chunk) => {
-            body += chunk;
-        });
-        req.on("end", () => {
-            let url = body;
-            // console.log(data);
-            // const url = 'https://facebook.com'
-            async function getMeta() {
-                try {
-                    const response = await axios_1.default.get(url);
-                    // console.log(response);
-                    const $ = cheerio.load(response.data);
-                    let obj = {
-                        title: "",
-                        description: "",
-                        image_url: ""
-                    };
-                    obj['title'] = $("title").text();
-                    obj['description'] = $('meta[name="description"]').attr('content');
-                    obj['image_url'] = $('meta[property="og:image"]').attr('content');
-                    console.log(obj);
-                    res.end(JSON.stringify(obj, null, 2));
-                }
-                catch (error) {
-                    console.error(error);
-                }
-            }
-            getMeta();
-        });
-    }
-    else {
-        res.writeHead(404, { 'Content-Type': 'text/HTML' })
-            .end(JSON.stringify({ alert: "Route Unavailable" }));
+const html_metadata_parser_1 = require("html-metadata-parser");
+const server = http_1.default.createServer(async (req, res) => {
+    if (req.url === "/" && req.method === "GET") {
+        const result = await html_metadata_parser_1.parser("https://www.linkedIn.com/");
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(JSON.stringify(result, null, 2));
+        console.log(JSON.stringify(result, null, 2));
     }
 });
-server.listen(3001, function () {
-    console.log('Server is very much running at port 3001');
-});
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- server2/server2/app.ts
+++ server2/server2/app.ts
@@ -1,50 +1,14 @@
-import axios from "axios";
-let cheerio = require("cheerio")
-
-// import cheerio from "cheerio"
 import http, { IncomingMessage, Server, ServerResponse } from "http";
+import { parser } from "html-metadata-parser"
 
-const server: Server = http.createServer(
-  (req: IncomingMessage, res: ServerResponse) => {
-    if (req.method === "POST" ) {
-      let body = "";
-                  req.on("data", (chunk) => {
-                    body += chunk;
-                  });
-                  req.on("end", () => {
-                    let url = body;
-                    // console.log(data);
-// const url = 'https://facebook.com'
-async function getMeta() {
-  try {
-      interface Data {
-        title?: string,
-        description? : string,
-        image_url?: string
-      }
-      const response = await axios.get(url);
-      // console.log(response);
-      const $ = cheerio.load(response.data);
-      let obj: Data = {
-        title: "",
-        description: "",
-        image_url: ""
-      };
-          obj['title'] = $("title").text();
-          obj['description'] = $('meta[name="description"]').attr('content')
-          obj['image_url'] = $('meta[property="og:image"]').attr('content')
-          console.log(obj);
-      res.end(JSON.stringify(obj, null, 2))
-  } catch (error) {
-      console.error(error);
+const server: Server = http.createServer(async (req: IncomingMessage, res: ServerResponse) => {
+    if (req.url ==="/" && req.method === "GET") {
+      const result = await parser("https://www.linkedIn.com/");
+      res.writeHead(200, {"Content-Type": "text/html"});
+      res.end(JSON.stringify(result, null,2));
+      console.log(JSON.stringify(result, null,2))
+    }
   }
-  } getMeta();
-});
-} else {
-  res.writeHead(404, {'Content-Type': 'text/HTML'})
-  .end(JSON.stringify({alert: "Route Unavailable"}))
-}
-  })
-server.listen(3001, function (){
-  console.log('Server is very much running at port 3001')
-});
+);
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
